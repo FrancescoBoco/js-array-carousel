e4a57228c0ea47d4c1faf157af635000e2ef997a
FrancescoBoco <francesco.bocoux@gmail.com>
2023-06-16 16:54:18 +0200

    inserito le immagini in HTMl

--- js/script.js
+++ js/script.js
@@ -3,31 +3,31 @@
 const album = [
     ` 
     <div class="item ">
-    <img src=":/img/01.webp" alt=""> 
+    <img src="img/01.webp" alt=""> 
     </div> 
     `
     ,
     ` 
     <div class="item ">
-    <img src=":/img/02.webp" alt=""> 
+    <img src="img/02.webp" alt=""> 
     </div> 
     `
     ,
     ` 
     <div class="item ">
-    <img src=":/img/03.webp" alt=""> 
+    <img src="img/03.webp" alt=""> 
     </div> 
     `
     ,
     ` 
     <div class="item ">
-    <img src=":/img/04.webp" alt=""> 
+    <img src="img/04.webp" alt=""> 
     </div> 
     `
     ,
     ` 
     <div class="item ">
-    <img src=":/img/05.webp" alt=""> 
+    <img src="img/05.webp" alt=""> 
     </div> 
     `
    
@@ -35,7 +35,10 @@ const album = [
 
 console.log ('album', album, typeof album)
 
+let items = document.getElementById("items")
+
 for (let i = 0; i < album.length; i++) {
     console. log ('-', i, album[i]);
-    document.getElementById('items').innerHTML = (album)
+    // document.getElementById('items').innerHTML = (album)
+    items.innerHTML += album[i];
 }
